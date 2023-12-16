97b77b084594b4577430ed831e6e2cad42678ac9
hyl <17803000829@163.com>
2023-12-17 01:07:37 +0800

    feat:表单设计器+

--- src/components/FormPlus/formDesign/Current/formOptions.js
+++ src/components/FormPlus/formDesign/Current/formOptions.js
@@ -1,6 +1,19 @@
 export default {
   items: [
     { label: "label宽度", component: "input", name: "labelWidth" },
-    { label: "label对齐方式", component: "input", name: "labelAlign" },
+    {
+      label: "label对齐方式",
+      component: "radio",
+      name: "labelAlign",
+      props: {
+        mode: "static",
+        options: [
+          { label: "左对齐", value: "left" },
+          { label: "居上", value: "top" },
+          { label: "右对齐", value: "right" },
+        ],
+      },
+      initialValue: "right",
+    },
   ],
 };
