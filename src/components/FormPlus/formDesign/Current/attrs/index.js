ecf4f7020443211aad187f1509474bd4bb157f0b
心念 <83170875+xinnian999@users.noreply.github.com>
2023-12-18 11:27:42 +0800

    Delete src/components/FormPlus/formDesign directory

--- src/components/FormPlus/formDesign/Current/attrs/index.js
+++
@@ -1,7 +0,0 @@
-export { default as input } from "./input";
-export { default as select } from "./select";
-export { default as radio } from "./radio";
-export { default as card } from "./card";
-export { default as inputNumber } from "./inputNumber";
-export { default as switch } from "./switch";
-export { default as formList } from "./formList";
