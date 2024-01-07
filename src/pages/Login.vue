05ea3a2f12ed6c29482eb9f39e33e65e231d172f
hyl <17803000829@163.com>
2024-01-07 22:20:22 +0800

    表单管理+

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,42 +5,14 @@
         <span class="title">心 念 Blog 后 台 管 理</span>
       </template>
       <div class="form">
-        <el-form :model="form" ref="formRef">
-          <el-form-item
-            label="用户名"
-            prop="username"
-            label-width="80px"
-            :rules="{ required: true, message: '请输入用户名！' }"
-          >
-            <el-input v-model="form.username" />
-          </el-form-item>
-          <el-form-item
-            label="密码"
-            prop="password"
-            label-width="80px"
-            :rules="{ required: true, message: '请输入密码！' }"
-          >
-            <el-input
-              v-model="form.password"
-              type="password"
-              autocomplete="off"
-            />
-          </el-form-item>
-
-          <div class="loginBtn">
-            <el-button type="primary" @click="submitForm" :loading="loading"
-              >登 录</el-button
-            >
-            <el-button @click="visitor" color="#626aef">游 客</el-button>
-          </div>
-        </el-form>
+        <schema-form :schema="schema" @onSubmit="onSubmit" />
       </div>
     </el-card>
   </div>
 </template>
 
 <script setup lang="jsx">
-import { reactive, ref } from "vue";
+import { ref, onMounted } from "vue";
 import { ElMessage } from "element-plus";
 import { request } from "@/utils";
 import { useRouter, useRoute } from "vue-router";
@@ -48,36 +20,28 @@ import md5 from "js-md5";
 
 const router = useRouter();
 const route = useRoute();
-const formRef = ref();
 const loading = ref(false);
 
-const form = reactive({
-  username: "",
-  password: "",
-});
+const schema = ref();
 
-const submitForm = () => {
-  formRef.value.validate((valid) => {
-    if (valid) {
-      loading.value = true;
-      request
-        .post("/user/loginAdmin", { ...form, password: md5(form.password) })
-        .then((res) => {
-          if (res.status === 0) {
-            ElMessage.success("登陆成功");
-            if (route.query.auth === "0") {
-              // 可能需要回到跳转登陆页前的页面
-              router.go(-1);
-            } else {
-              router.push("/home");
-            }
-          } else {
-            loading.value = false;
-            ElMessage.error("登陆失败");
-          }
-        });
-    }
-  });
+const onSubmit = (values) => {
+  loading.value = true;
+  request
+    .post("/user/loginAdmin", { ...values, password: md5(values.password) })
+    .then((res) => {
+      if (res.status === 0) {
+        ElMessage.success("登陆成功");
+        if (route.query.auth === "0") {
+          // 可能需要回到跳转登陆页前的页面
+          router.go(-1);
+        } else {
+          router.push("/home");
+        }
+      } else {
+        loading.value = false;
+        ElMessage.error("登陆失败");
+      }
+    });
 };
 
 const visitor = () => {
@@ -94,6 +58,19 @@ const visitor = () => {
       }
     });
 };
+
+onMounted(async () => {
+  const { data } = await request({
+    url: "/current/query/form",
+    params: {
+      filters: {
+        id: 3,
+      },
+    },
+  });
+
+  schema.value = JSON.parse(data[0].formSchema);
+});
 </script>
 
 <style lang="less">
--- src/pages/formDesign.vue
+++ src/pages/formDesign.vue
@@ -1,27 +1,45 @@
 <template>
-  <form-design />
+  <form-design v-model="schema" @onSave="onSave" />
 </template>
 
 <script setup lang="jsx">
 import { useRoute } from "vue-router";
 import { ref, onMounted } from "vue";
 import { request } from "@/utils";
+import { ElMessage } from "element-plus";
 
 const route = useRoute();
 
-const schema = ref({});
+const schema = ref();
 
 onMounted(async () => {
-  const { data } = await request({
-    url: "/current/query/form",
-    params: {
-      filters: {
-        id: +route.query.formId,
+  if (route.query.formId) {
+    const { data } = await request({
+      url: "/current/query/form",
+      params: {
+        filters: {
+          id: +route.query.formId,
+        },
       },
+    });
+    schema.value = JSON.parse(data[0].formSchema);
+  }
+});
+
+const onSave = async () => {
+  if (!route.query.formId) {
+    return ElMessage.error(`未指定表单id`);
+  }
+
+  await request({
+    url: "/current/update/form",
+    method: "put",
+    data: {
+      id: +route.query.formId,
+      formSchema: JSON.stringify(schema.value),
     },
   });
-  schema.value = data[0].formSchema;
 
-  console.log(schema.value);
-});
+  ElMessage.success(`保存成功`);
+};
 </script>
